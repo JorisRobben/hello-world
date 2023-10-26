24b41d62a5217a6806aebb9b68496ee066def16c
JorisRobben <149020196+JorisRobben@users.noreply.github.com>
2023-10-26 16:41:33 +0200

    Update main(1).py

--- main(1).py
+++ main(1).py
@@ -3,10 +3,11 @@ def printTable(someTable):
   numCols = len(someTable[0])
   longestStringLength = 0
   
-  for i in range(0,len(someTable)):
-    currentStringLength = len(someTable[i][0]) + len(someTable[i][1]) + len(someTable[i][2])
-    
-  if currentStringLength > longestStringLength:
-    longestStringLength = currentStringLength
+  for i in range(0,len(someTable[0])):
+    currentStringLength = 0
+    for j in range(0,len(someTable)):
+      currentStringLength = currentStringLength + len(someTable[j][i])
+    if currentStringLength > longestStringLength:
+      longestStringLength = currentStringLength
   print(str(longestStringLength))
-printTable([['apples', 'oranges', 'cherries','banana'],['Alice','Bob','Carol','David'],['dogs','cats','moose','goose']])
+printTable([['apples', 'oranges', 'che','ban'],['Alice','Bobbutlonger','Carol','David'],['dogs','cats','moose','goose']])
